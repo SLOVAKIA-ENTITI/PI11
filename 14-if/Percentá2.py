0fab885206daa0c09f9f8f75b6be21114cb297f5
Matúš Paškala <mpaskala08@gmail.com>
2023-12-13 11:03:33 +0100

    Pridanie počítania percent z počtu bodov.

--- 14-if/Percentá2.py
+++ 14-if/Percentá2.py
@@ -1,5 +1,7 @@
-percenta = int(input("Zadaj percentá:"))
-
+max_bodov = 30
+pocet_bodov = int(input("Zadaj svoj počet bodov za test."))
+percenta = (pocet_bodov / max_bodov) * 100
+print(f"Získal si {percenta}%")
 if percenta >= 90:
     print("Výborný.")
 elif 75 <= percenta:
